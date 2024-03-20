e0ad1b6917cb1bc5de4da9cca2d4aa6041970b09
David Vilela <dvilelaf@gmail.com>
2024-03-20 16:27:40 +0100

    fix: packages

--- packages/celo/skills/celo_swapper_chained_abci/__init__.py
+++ packages/celo/skills/celo_swapper_chained_abci/__init__.py
@@ -22,4 +22,4 @@
 from aea.configurations.base import PublicId
 
 
-PUBLIC_ID = PublicId.from_str("celo/celo_swapper_chained:0.1.0")
+PUBLIC_ID = PublicId.from_str("celo/celo_swapper_chained_abci:0.1.0")
